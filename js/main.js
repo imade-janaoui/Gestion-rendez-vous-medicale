4b8d846aa9628b9661ea4a848c7b2952696aaa55
imade-janaoui <imadejanaoui@gmail.com>
2022-01-04 14:30:11 +0100

    ferst commit

---
+++ js/main.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {  
+    var gsearch = function() {  
+     var url = 'https://www.google.com/search?q=' + document.getElementById('searchme').value;  
+     window.open(url, 'google');  
+    };  
+    var glucky = function() {  
+     var url = 'https://www.google.com/search?q=' + document.getElementById('searchme').value + '&btnI';  
+     window.open(url, 'google');  
+    };  
+    $(".search-btn").click(gsearch);  
+    $(".right").click(glucky);  
+    $("#searchme").keypress(function(e) {  
+     if (e.which == 13) {  
+      gsearch();  
+     }  
+    });  
+   });  
